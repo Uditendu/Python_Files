39c68603f21228eb8aa626cc463c7fe31e475600
Uditendu Mukhopadhyay <u.mukhopadhyay@tudelft.nl>
2015-11-04 14:03:30 +0000

    Initial Commit. Two python modules

---
+++ Count_Occurance.py
@@ -0,0 +1,20 @@
+"""Module to count how many times check appears in a String
+"""
+def count_occurance(txt, check):
+    Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-'
+    txt+=' '
+    l = len(check)
+    n=0
+    i=0
+    j=0
+
+    while i<(len(txt)-l+1):
+        if txt[i]==check[0]:
+            if txt[i:i+l]==check and txt[i-1].upper() not in Alphabet and txt[i+l].upper() not in Alphabet:
+                n+=1
+                i+=l
+            else:
+                i+=1
+        else:
+            i+=1
+    return "No. of Occurance of "+check+"="+str(n)
---
+++ Word_Count.py
@@ -0,0 +1,27 @@
+def word_count(txt):
+    Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-'
+    txt+=' '
+    i=0
+    j=0
+    if txt[0].upper() in Alphabet:
+        n=1
+    else:
+        n=0
+
+    while i< len(txt):
+        if txt[i].upper() not in Alphabet:
+            j=i
+            while txt[j].upper() not in Alphabet:
+                if j == (len(txt)-1):
+                    break
+                else:
+                    j+=1
+            if j == (len(txt)-1):
+                    break 
+            i=j
+            n+=1
+        elif i == (len(txt)-1):
+            break
+        else:
+            i+=1
+    return "no. of words=" + str(n)
