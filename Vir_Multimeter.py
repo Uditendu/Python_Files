e52688c4c201c230de7f713623e9d96c37c9ab4f
Uditendu Mukhopadhyay <u.mukhopadhyay@tudelft.nl>
2015-11-06 12:02:47 +0000

    2nd modified version of the multimeter

--- Vir_Multimeter.py
+++ Vir_Multimeter.py
@@ -1,4 +1,5 @@
 import socket
+import numpy as np
 
 class Multimeter(object):
     def __init__(self, host, port, bufsize):
@@ -12,8 +13,10 @@ class Multimeter(object):
             try:
                 mydata = float(input(A))
                 if mydata>1.0:
-                    print ('You are going to burn your instrument')
-                    k+=1
+                    print ('You are going to burn your instrument: Please enter a value between 0 to 1')
+                    k+=1 
+                elif mydata<0.0:
+                    print ('Please give a positive current between 0 to 1')
                 else:
                     value = mydata
                     break
@@ -21,38 +24,49 @@ class Multimeter(object):
                 print ('Current is a number, Moron')
                 k+=1
         return value
-
-    def Read_Voltage(self):
+    
+    def Set_Current_Value(self,val):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((self.host, self.port))
-        r=''
+        message=bytes('SET:CUR'+str(val) ,'utf-8')
+        r = self.bypass_error(message,3)
+        self.sock.shutdown(2)
+        self.sock.close()
+        return r
+    
+    def bypass_error(self,message,m):
         v = 0
+        r = ''
         n = 0
-        message=bytes('READ:VOL' ,'utf-8')
         while v==0:
             self.sock.send(message)
             r = self.sock.recv(self.bufsize)
             c = str(r)
             try:
-                x = int(str(c[-3]))
+                x = int(str(c[-m]))
                 v=1
                 n+=1
             except ValueError:
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.sock.connect((self.host, self.port))
                 v=0
+        return r
+
+    def Read_Voltage(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host, self.port))
+        message=bytes('READ:VOL' ,'utf-8')
+        r = self.bypass_error(message,3)
         self.sock.shutdown(2)
         self.sock.close()
-        print('No. of tries for voltage='+str(n))
+        #print('No. of tries for voltage='+str(n))
         return r
     
     def Read_Resistance(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((self.host, self.port))
-        r=''
         message=bytes('READ:RES' ,'utf-8')
-        self.sock.send(message)
-        r = self.sock.recv(self.bufsize)
+        r = self.bypass_error(message,5)
         self.sock.shutdown(2)
         self.sock.close()
         return r
@@ -60,25 +74,16 @@ class Multimeter(object):
     def Read_Current(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((self.host, self.port))
-        r=''
         message=bytes('READ:CUR' ,'utf-8')
-        self.sock.send(message)
-        r = self.sock.recv(self.bufsize)
+        r = self.bypass_error(message,3)
         self.sock.shutdown(2)
         self.sock.close()
         return r
     
     def Set_Current(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.host, self.port))
-        r=''
-        value = self.Cheak_Current_Val('Please enter the desired value (in Amp):')
-        message=bytes('SET:CUR'+str(value) ,'utf-8')
-        self.sock.send(message)
-        r = self.sock.recv(self.bufsize)
-        self.sock.shutdown(2)
-        self.sock.close()
-        return r
+        val = self.Cheak_Current_Val('Please enter the desired value (in Amp):')
+        cur = self.Set_Current_Value(val)
+        return cur
     
     def Meas_R_Single_Point(self):
         i = str(self.Set_Current())
@@ -93,4 +98,21 @@ class Multimeter(object):
         fin = self.Cheak_Current_Val('Please enter the final current value (in Amp):')
         diff = self.Cheak_Current_Val('Please enter the current step (in Amp):')
         
+        current = ini
+        X = []
+        Y = []
+        Z = []
         
+        while (fin)>current:
+            i = str(self.Set_Current_Value(current))
+            v = str(self.Read_Voltage())
+            print(i)
+            print(v)
+            r = float(v[11:-2])/float(i[11:-2])
+            X.append(float(i[11:-2]))
+            Y.append(float(v[11:-2]))
+            Z.append(r)
+            current+=diff
+            
+        
+        return Z
